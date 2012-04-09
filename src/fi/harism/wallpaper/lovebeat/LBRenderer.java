80341af519c67eeaabfe607751d34319db754933
harism <harism@gmail.com>
2012-04-09 15:32:32 +0300

    Copyright owner name changed to US-ASCII characters only.

--- src/fi/harism/wallpaper/lovebeat/LBRenderer.java
+++ src/fi/harism/wallpaper/lovebeat/LBRenderer.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2012 Harri Smått
+   Copyright 2012 Harri Smatt
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
